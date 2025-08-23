9e16f40ba1940aa8c62370cb8586b21134cc0c76
Rhian Jared Boncajes <rhianjaredboncajes@gmail.com>
2025-08-23 11:52:18 +0800

    Calculato

---
+++ S02/activity/A01_Calculator/A01_Calculator/Form1.Designer.cs
@@ -0,0 +1,396 @@
+﻿namespace A01_Calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            textEquation = new TextBox();
+            textDisplay = new TextBox();
+            btnEqual = new Button();
+            btnMinus = new Button();
+            btnDivide = new Button();
+            btnTimes = new Button();
+            btnErase = new Button();
+            btnDelete = new Button();
+            btnClear = new Button();
+            btn9 = new Button();
+            btn8 = new Button();
+            btn7 = new Button();
+            btn6 = new Button();
+            btn5 = new Button();
+            btn4 = new Button();
+            btn3 = new Button();
+            btnPlus = new Button();
+            btn2 = new Button();
+            btnDot = new Button();
+            btn1 = new Button();
+            btn0 = new Button();
+            btnSettings = new Button();
+            txtHistory = new TextBox();
+            lblHistory = new Label();
+            SuspendLayout();
+            // 
+            // textEquation
+            // 
+            textEquation.BackColor = Color.Silver;
+            textEquation.BorderStyle = BorderStyle.FixedSingle;
+            textEquation.Font = new Font("Impact", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textEquation.Location = new Point(12, 32);
+            textEquation.Name = "textEquation";
+            textEquation.Size = new Size(204, 31);
+            textEquation.TabIndex = 0;
+            // 
+            // textDisplay
+            // 
+            textDisplay.BorderStyle = BorderStyle.FixedSingle;
+            textDisplay.Font = new Font("Impact", 20.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textDisplay.Location = new Point(12, 71);
+            textDisplay.Name = "textDisplay";
+            textDisplay.Size = new Size(204, 40);
+            textDisplay.TabIndex = 1;
+            // 
+            // btnEqual
+            // 
+            btnEqual.BackColor = Color.FromArgb(0, 192, 192);
+            btnEqual.Font = new Font("Impact", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnEqual.Location = new Point(170, 302);
+            btnEqual.Name = "btnEqual";
+            btnEqual.Size = new Size(46, 38);
+            btnEqual.TabIndex = 11;
+            btnEqual.Text = "=";
+            btnEqual.UseVisualStyleBackColor = false;
+            btnEqual.Click += btnEqual_Click;
+            // 
+            // btnMinus
+            // 
+            btnMinus.BackColor = Color.Red;
+            btnMinus.Font = new Font("Impact", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnMinus.Location = new Point(170, 258);
+            btnMinus.Name = "btnMinus";
+            btnMinus.Size = new Size(46, 38);
+            btnMinus.TabIndex = 12;
+            btnMinus.Text = "-";
+            btnMinus.UseVisualStyleBackColor = false;
+            btnMinus.Click += Operator;
+            // 
+            // btnDivide
+            // 
+            btnDivide.BackColor = Color.FromArgb(0, 192, 0);
+            btnDivide.Font = new Font("Impact", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnDivide.Location = new Point(170, 214);
+            btnDivide.Name = "btnDivide";
+            btnDivide.Size = new Size(46, 38);
+            btnDivide.TabIndex = 13;
+            btnDivide.Text = "/";
+            btnDivide.UseVisualStyleBackColor = false;
+            btnDivide.Click += Operator;
+            // 
+            // btnTimes
+            // 
+            btnTimes.BackColor = Color.Purple;
+            btnTimes.Font = new Font("Impact", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnTimes.Location = new Point(170, 170);
+            btnTimes.Name = "btnTimes";
+            btnTimes.Size = new Size(46, 38);
+            btnTimes.TabIndex = 14;
+            btnTimes.Text = "*";
+            btnTimes.UseVisualStyleBackColor = false;
+            btnTimes.Click += Operator;
+            // 
+            // btnErase
+            // 
+            btnErase.BackColor = Color.FromArgb(255, 128, 0);
+            btnErase.Font = new Font("Impact", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnErase.Location = new Point(170, 126);
+            btnErase.Name = "btnErase";
+            btnErase.Size = new Size(46, 38);
+            btnErase.TabIndex = 15;
+            btnErase.Text = "⌫";
+            btnErase.UseVisualStyleBackColor = false;
+            // 
+            // btnDelete
+            // 
+            btnDelete.BackColor = Color.Red;
+            btnDelete.Font = new Font("Impact", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnDelete.Location = new Point(118, 126);
+            btnDelete.Name = "btnDelete";
+            btnDelete.Size = new Size(46, 38);
+            btnDelete.TabIndex = 16;
+            btnDelete.Text = "C";
+            btnDelete.UseVisualStyleBackColor = false;
+            // 
+            // btnClear
+            // 
+            btnClear.BackColor = Color.Yellow;
+            btnClear.Font = new Font("Impact", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnClear.Location = new Point(66, 126);
+            btnClear.Name = "btnClear";
+            btnClear.Size = new Size(46, 38);
+            btnClear.TabIndex = 17;
+            btnClear.Text = "CE";
+            btnClear.UseVisualStyleBackColor = false;
+            // 
+            // btn9
+            // 
+            btn9.BackColor = Color.FromArgb(0, 0, 192);
+            btn9.Font = new Font("Impact", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btn9.Location = new Point(118, 170);
+            btn9.Name = "btn9";
+            btn9.Size = new Size(46, 38);
+            btn9.TabIndex = 18;
+            btn9.Text = "9";
+            btn9.UseVisualStyleBackColor = false;
+            btn9.Click += Load_Key;
+            // 
+            // btn8
+            // 
+            btn8.BackColor = Color.Teal;
+            btn8.Font = new Font("Impact", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btn8.Location = new Point(66, 170);
+            btn8.Name = "btn8";
+            btn8.Size = new Size(46, 38);
+            btn8.TabIndex = 19;
+            btn8.Text = "8";
+            btn8.UseVisualStyleBackColor = false;
+            btn8.Click += Load_Key;
+            // 
+            // btn7
+            // 
+            btn7.BackColor = Color.Green;
+            btn7.Font = new Font("Impact", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btn7.Location = new Point(14, 170);
+            btn7.Name = "btn7";
+            btn7.Size = new Size(46, 38);
+            btn7.TabIndex = 20;
+            btn7.Text = "7";
+            btn7.UseVisualStyleBackColor = false;
+            btn7.Click += Load_Key;
+            // 
+            // btn6
+            // 
+            btn6.BackColor = Color.Yellow;
+            btn6.Font = new Font("Impact", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btn6.Location = new Point(118, 214);
+            btn6.Name = "btn6";
+            btn6.Size = new Size(46, 38);
+            btn6.TabIndex = 21;
+            btn6.Text = "6";
+            btn6.UseVisualStyleBackColor = false;
+            btn6.Click += Load_Key;
+            // 
+            // btn5
+            // 
+            btn5.BackColor = Color.FromArgb(255, 128, 0);
+            btn5.Font = new Font("Impact", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btn5.Location = new Point(66, 214);
+            btn5.Name = "btn5";
+            btn5.Size = new Size(46, 38);
+            btn5.TabIndex = 22;
+            btn5.Text = "5";
+            btn5.UseVisualStyleBackColor = false;
+            btn5.Click += Load_Key;
+            // 
+            // btn4
+            // 
+            btn4.BackColor = Color.Red;
+            btn4.Font = new Font("Impact", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btn4.Location = new Point(14, 214);
+            btn4.Name = "btn4";
+            btn4.Size = new Size(46, 38);
+            btn4.TabIndex = 23;
+            btn4.Text = "4";
+            btn4.UseVisualStyleBackColor = false;
+            btn4.Click += Load_Key;
+            // 
+            // btn3
+            // 
+            btn3.BackColor = Color.Fuchsia;
+            btn3.Font = new Font("Impact", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btn3.Location = new Point(118, 258);
+            btn3.Name = "btn3";
+            btn3.Size = new Size(46, 38);
+            btn3.TabIndex = 24;
+            btn3.Text = "3";
+            btn3.UseVisualStyleBackColor = false;
+            btn3.Click += Load_Key;
+            // 
+            // btnPlus
+            // 
+            btnPlus.BackColor = Color.Lime;
+            btnPlus.Font = new Font("Impact", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnPlus.Location = new Point(118, 302);
+            btnPlus.Name = "btnPlus";
+            btnPlus.Size = new Size(46, 38);
+            btnPlus.TabIndex = 25;
+            btnPlus.Text = "+";
+            btnPlus.UseVisualStyleBackColor = false;
+            btnPlus.Click += Operator;
+            // 
+            // btn2
+            // 
+            btn2.BackColor = Color.Blue;
+            btn2.Font = new Font("Impact", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btn2.Location = new Point(66, 258);
+            btn2.Name = "btn2";
+            btn2.Size = new Size(46, 38);
+            btn2.TabIndex = 26;
+            btn2.Text = "2";
+            btn2.UseVisualStyleBackColor = false;
+            btn2.Click += Load_Key;
+            // 
+            // btnDot
+            // 
+            btnDot.BackColor = Color.Yellow;
+            btnDot.Font = new Font("Impact", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnDot.Location = new Point(66, 302);
+            btnDot.Name = "btnDot";
+            btnDot.Size = new Size(46, 38);
+            btnDot.TabIndex = 27;
+            btnDot.Text = ".";
+            btnDot.UseVisualStyleBackColor = false;
+            btnDot.Click += Load_Key;
+            // 
+            // btn1
+            // 
+            btn1.BackColor = Color.FromArgb(0, 192, 192);
+            btn1.Font = new Font("Impact", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btn1.Location = new Point(14, 258);
+            btn1.Name = "btn1";
+            btn1.Size = new Size(46, 38);
+            btn1.TabIndex = 28;
+            btn1.Text = "1";
+            btn1.UseVisualStyleBackColor = false;
+            btn1.Click += Load_Key;
+            // 
+            // btn0
+            // 
+            btn0.BackColor = Color.FromArgb(255, 128, 0);
+            btn0.Font = new Font("Impact", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btn0.Location = new Point(14, 302);
+            btn0.Name = "btn0";
+            btn0.Size = new Size(46, 38);
+            btn0.TabIndex = 29;
+            btn0.Text = "0";
+            btn0.UseVisualStyleBackColor = false;
+            btn0.Click += Load_Key;
+            // 
+            // btnSettings
+            // 
+            btnSettings.Font = new Font("Impact", 8.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnSettings.Location = new Point(183, 3);
+            btnSettings.Name = "btnSettings";
+            btnSettings.Size = new Size(33, 23);
+            btnSettings.TabIndex = 30;
+            btnSettings.Text = "☰";
+            btnSettings.UseVisualStyleBackColor = true;
+            btnSettings.Click += btnSettings_Click;
+            // 
+            // txtHistory
+            // 
+            txtHistory.BackColor = Color.FromArgb(192, 192, 0);
+            txtHistory.Location = new Point(222, 32);
+            txtHistory.Multiline = true;
+            txtHistory.Name = "txtHistory";
+            txtHistory.Size = new Size(129, 308);
+            txtHistory.TabIndex = 31;
+            // 
+            // lblHistory
+            // 
+            lblHistory.AutoSize = true;
+            lblHistory.Font = new Font("Wide Latin", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblHistory.Location = new Point(225, 12);
+            lblHistory.Name = "lblHistory";
+            lblHistory.Size = new Size(121, 15);
+            lblHistory.TabIndex = 32;
+            lblHistory.Text = "HISTORY";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Salmon;
+            ClientSize = new Size(355, 352);
+            Controls.Add(lblHistory);
+            Controls.Add(txtHistory);
+            Controls.Add(btnSettings);
+            Controls.Add(btn0);
+            Controls.Add(btn1);
+            Controls.Add(btnDot);
+            Controls.Add(btn2);
+            Controls.Add(btnPlus);
+            Controls.Add(btn3);
+            Controls.Add(btn4);
+            Controls.Add(btn5);
+            Controls.Add(btn6);
+            Controls.Add(btn7);
+            Controls.Add(btn8);
+            Controls.Add(btn9);
+            Controls.Add(btnClear);
+            Controls.Add(btnDelete);
+            Controls.Add(btnErase);
+            Controls.Add(btnTimes);
+            Controls.Add(btnDivide);
+            Controls.Add(btnMinus);
+            Controls.Add(btnEqual);
+            Controls.Add(textDisplay);
+            Controls.Add(textEquation);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            Name = "Form1";
+            Text = "CALCULATOR";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox textEquation;
+        private TextBox textDisplay;
+        private Button btnEqual;
+        private Button btnMinus;
+        private Button btnDivide;
+        private Button btnTimes;
+        private Button btnErase;
+        private Button btnDelete;
+        private Button btnClear;
+        private Button btn9;
+        private Button btn8;
+        private Button btn7;
+        private Button btn6;
+        private Button btn5;
+        private Button btn4;
+        private Button btn3;
+        private Button btnPlus;
+        private Button btn2;
+        private Button btnDot;
+        private Button btn1;
+        private Button btn0;
+        private Button btnSettings;
+        private TextBox txtHistory;
+        private Label lblHistory;
+    }
+}
---
+++ S02/activity/A01_Calculator/A01_Calculator/Form1.cs
@@ -0,0 +1,162 @@
+using System.Data.OleDb;
+
+namespace A01_Calculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        bool isNewEntry = false;
+        private int originalWidth;
+        double firstNumber = 0;
+        string operation = "";
+        string dbPath = @"C:\LOCALDB\Calculator.mdb";
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            this.Width = 237;
+
+            //original terminal width when the form loads
+            originalWidth = this.Width;
+        }
+
+        private void btnSettings_Click(object sender, EventArgs e)
+        {
+            if (this.Width >= 370)
+            {
+                //Return to original width
+                this.Width = originalWidth;
+            }
+            else
+            {
+                //Expand width to 370
+                this.Width = 370;
+            }
+        }
+        private void Load_Key(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+
+            if (isNewEntry)
+            {
+                textDisplay.Text = "";
+                isNewEntry = false;
+            }
+            //Avoid leading zero
+            if (textDisplay.Text == "0" && btn.Text != ".")
+            {
+                textDisplay.Text = "";
+            }
+
+            int dotCount = textDisplay.Text.Count(c => c == '.');
+            if (dotCount >= 1)
+            {
+                if (btn.Text != ".")
+                {
+                    textDisplay.Text += btn.Text;
+                    textEquation.Text += btn.Text;
+                }
+            }
+            else if (textDisplay.Text == "" && btn.Text == ".")
+            {
+                textDisplay.Text = "0.";
+                textEquation.Text = "0.";
+            }
+            else
+            {
+                textDisplay.Text += btn.Text;
+                if (textDisplay.Text.Contains(".."))
+                {
+                    textDisplay.Text = textDisplay.Text.Replace("..", ".");
+                }
+                textEquation.Text += btn.Text;
+            }
+        }
+        private void Operator(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+
+            if (double.TryParse(textDisplay.Text, out firstNumber))
+            {
+                operation = btn.Text;
+                isNewEntry = true;
+
+                textEquation.Text = firstNumber.ToString() + " " + operation + " ";
+            }
+        }
+        private void Total()
+        {
+            double secondNumber;
+
+            double result = 0;
+
+            if (!double.TryParse(textDisplay.Text, out secondNumber))
+                return;
+
+            if (operation == "+")
+            {
+                result = firstNumber + secondNumber;
+            }
+
+            else if (operation == "-")
+            {
+                result = firstNumber - secondNumber;
+            }
+
+            else if (operation == "*")
+            {
+                result = firstNumber * secondNumber;
+            }
+
+            else if (operation == "/")
+            {
+                if (secondNumber == 0)
+                {
+                    MessageBox.Show("Cannot divide by zero");
+                    return;
+                }
+                result = firstNumber / secondNumber;
+            }
+            textDisplay.Text = result.ToString();
+
+            textEquation.Text += "= " + result.ToString();
+            InsertEquationToDatabase(textEquation.Text);
+
+        }
+
+        private void btnEqual_Click(object sender, EventArgs e)
+        {
+            Total();
+        }
+
+        private void InsertEquationToDatabase(string equationText)
+        {
+            string connStr = $"Provider=Microsoft.Jet.OLEDB.4.0;Data Source={dbPath};";
+
+            string insertQuery = "INSERT INTO tbl_Calculator_History (Equation) VALUES (@equation)";
+
+            using (OleDbConnection conn = new OleDbConnection(connStr))
+            {
+                try
+                {
+                    conn.Open();
+                    OleDbCommand insertCmd = new OleDbCommand(insertQuery, conn);
+                    insertCmd.Parameters.AddWithValue("@equation", equationText);
+                    insertCmd.ExecuteNonQuery();
+
+                    conn.Close();
+                }
+
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Insert error:" + ex.Message);
+                }
+            }
+        }
+
+
+    }
+}
